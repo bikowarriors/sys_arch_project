de821bf52806ba626b79b84f3410e2ccb94d4806
Francis Lance Dinglasa <fldinglasa@gmail.com>
2024-05-27 15:55:46 +0800

    Added from the dashboard +special cakes customization

--- dash_script.js
+++ dash_script.js
@@ -1,4 +1,6 @@
 const categoryButtons = document.querySelectorAll('.category-button');
+const specialCakesButton = document.querySelector('.special-cakes-button');
+const specialCakesContainer = document.querySelector('.product-container[data-product-container="specialcakes"]');
 
 categoryButtons.forEach(button => {
   button.addEventListener('click', function() {
@@ -16,3 +18,16 @@ categoryButtons.forEach(button => {
     this.classList.add('active');
   });
 });
+
+specialCakesButton.addEventListener('click', function() {
+    // Deactivate all product containers
+    document.querySelectorAll('.product-container').forEach(container => container.classList.remove('active'));
+  
+    // Activate the special cakes product container
+    specialCakesContainer.classList.add('active');
+  
+    // (Optional) Remove active class from other category buttons
+    document.querySelectorAll('.category-button.active').forEach(button => button.classList.remove('active'));
+    // Add active class to the special cakes button
+    this.classList.add('active');
+  });
